1b9a648b8b703b08f0341a8829f8565f6075af09
Paul Jackson <paul@jwareservices.com>
2017-04-18 20:49:47 -0400

    Update RemoteException.java

--- src/main/java/org/jware/remote/RemoteException.java
+++ src/main/java/org/jware/remote/RemoteException.java
@@ -1,1 +1,47 @@
-package org.jware.remote;/* * Copyright (C) 2014 J. Paul Jackson <jwareservices@gmail.com> * * This program is free software: you can redistribute it and/or modify * it under the terms of the GNU General Public License as published by * the Free Software Foundation, either version 3 of the License, or * (at your option) any later version. * * This program is distributed in the hope that it will be useful, * but WITHOUT ANY WARRANTY; without even the implied warranty of * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License * along with this program.  If not, see <http://www.gnu.org/licenses/>. *//* * File: RemoteException.java * * @author J. Paul Jackson <jwareservices@gmail.com> *  * @version 1.0 July 1, 1998 *//* * RemoteExcpetion is an exception for the jware remote event model. */public class RemoteException extends java.lang.RuntimeException {    public RemoteException() {        super();    }    public RemoteException(String s) {        super(s);    }    public String toString() {        StringBuffer buff = new StringBuffer();        buff.append(super.toString());        return new String(buff);    }}
+package org.jware.remote;
+
+/*
+ * Copyright (C) 2014 J. Paul Jackson <jwareservices@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/*
+ * File: RemoteException.java
+ *
+ * @author J. Paul Jackson <jwareservices@gmail.com>
+ * 
+ * @version 1.0 July 1, 1998
+ */
+
+/*
+ * RemoteExcpetion is an exception for the jware remote event model.
+ */
+public class RemoteException extends java.lang.RuntimeException {
+
+    public RemoteException() {
+        super();
+    }
+
+    public RemoteException(String s) {
+        super(s);
+    }
+
+    public String toString() {
+        StringBuffer buff = new StringBuffer();
+        buff.append(super.toString());
+        return new String(buff);
+    }
+}
